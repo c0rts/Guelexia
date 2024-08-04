9235f73235ace909331cddacdb9ef918677e7af3
C0rts <matheuscbarbeiro30@gmail.com>
2024-08-04 17:04:07 -0300

    Projeto TCC todo

---
+++ SITE (PROGRAMAÇÃO)/Scripts/script_dic.js
@@ -0,0 +1,61 @@
+var botao = document.getElementById('bntconfirma');// Obtém uma referência ao botão
+botao.addEventListener('click', confirmaPlvr); // Adiciona um ouvinte de evento para o evento 'click'
+
+function confirmaPlvr(){
+    
+const palavra = document.getElementById('palavra').value.replace("-","");//palvra do html
+
+
+function verificarNumeros() {
+
+    // Transforma a palavra em um array de caracteres
+    var letras = palavra.split('');
+
+    // Verifica se há algum número em cada letra
+    var numeroEncontrado = false;
+
+    letras.forEach(letra => {
+        if (!isNaN(letra)) { // Verifica se a letra é um número
+
+            numeroEncontrado = true;
+        }
+    });
+    if(numeroEncontrado){
+        console.log('vai caga ta na calculadora não seu fedido')
+    }else{
+        pesquisaPlvr
+    }
+}
+
+
+const pesquisaPlvr = async() => {
+    limparFormulario();
+
+ 
+    const url = `https://significado.herokuapp.com/v2/${palavra}`;
+    if (Plvrconfirma(palavra)){
+        const dados = await fetch(url);
+        const resultado = await dados.json();
+        if (resultado.hasOwnProperty('erro')){
+            console.log('Vai se fode');//document.getElementById('cxsignificado').value = 'Palavra não encontrada!';
+        }else {
+            preencherFormulario(resultado);
+        }
+    }else{
+        console.log('Palavra incorreta')//document.getElementById('cxsignificado').value = 'Palavra incorreta!';
+    }
+     
+}
+
+}
+
+
+
+const preencherSignificados = (resultado) =>{
+    console.log()
+}
+    /*document.getElementById('endereco').value = endereco.logradouro;
+    document.getElementById('bairro').value = endereco.bairro;
+    document.getElementById('cidade').value = endereco.localidade;
+    document.getElementById('estado').value = endereco.uf;
+}*/
