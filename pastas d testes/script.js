f77f3f6495ad874e7a7684d4f64fca2831c77eec
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2024-08-05 07:06:39 -0300

    Adicinando lógica de quando clicar no botão certo ele adicionar ao contador os pontos

--- pastas d testes/script.js
+++ pastas d testes/script.js
@@ -16,40 +16,73 @@ function getRandomIndex(arr) {
 // Função para atualizar a imagem
 function updateImage() {
     const imageElement = document.getElementById('imagem_qst');
-    const imagen = imagens[getRandomIndex(imagens)];
-    const randomImage = imagen.caminho;
-    let respostas = [imagen.resposta];
-    pegarRespostas(respostas);
+    const imagem = imagens[getRandomIndex(imagens)];
+    const randomImage = imagem.caminho;
+    let respostasArray = [imagem.resposta];
+
+    pegarRespostas(respostasArray);
+    respostasBotoes(respostasArray);
+    respostaCerta(imagem.resposta);
+
     imageElement.src = randomImage;
-    Respostas_btn(respostas);
+
     contador.innerHTML = valorAcertos;
-    valorAcertos++;
 }
 
-function pegarRespostas(respostas) {
+function pegarRespostas(respostasArray) {
     // enquanto o tamanho do array de respostas for menor que 4 ele vai ficar fazendo essa rotina
-    while (respostas.length < 4) {
-        let imagen = imagens[getRandomIndex(imagens)];
-        if (!respostas.includes(imagen.resposta)) {
-            respostas.push(imagen.resposta);
+    while (respostasArray.length < 4) {
+        let imagem = imagens[getRandomIndex(imagens)];
+        if (!respostasArray.includes(imagem.resposta)) {
+            respostasArray.push(imagem.resposta);
         }
     }
 
     // Embaralhar as respostas 
-    respostas.sort(() => Math.random() - 0.5);
+    respostasArray.sort(() => Math.random() - 0.5);
 }
 
-function Respostas_btn(respostas) {
+function respostasBotoes(respostasArray) {
     // Seleciona os botões pelo ID e altera o texto deles
-    botao0.textContent = respostas[0];
-    botao1.textContent = respostas[1];
-    botao2.textContent = respostas[2];
-    botao3.textContent = respostas[3];
+    botao0.textContent = respostasArray[0];
+    botao1.textContent = respostasArray[1];
+    botao2.textContent = respostasArray[2];
+    botao3.textContent = respostasArray[3];
+}
+
+function removerRespostasBotoes() {
+
+    let botoes = [botao0, botao1, botao2, botao3];
+    botoes.forEach(botao => { // clona um botão e substitui o botão original pelo seu clone, isso faz com que remova o evento que o botão que foi clonado tinha
+        let novoBotao = botao.cloneNode(true);
+        botao.parentNode.replaceChild(novoBotao, botao);
+    });
+
+    // Reatribuir os novos botões aos identificadores
+    botao0 = document.getElementById('opcao_btn_0');
+    botao1 = document.getElementById('opcao_btn_1');
+    botao2 = document.getElementById('opcao_btn_2');
+    botao3 = document.getElementById('opcao_btn_3');
+}
+
+function respostaCerta(respostaCerta) {
+    removerRespostasBotoes();
+
+    let botoes = [botao0, botao1, botao2, botao3];
+
+    botoes.forEach(botao => {
+        if (botao.textContent == respostaCerta) { // se o botão clicado for o certo ele adiciona ao contador
+            botao.addEventListener('click', () => {
+                valorAcertos++;
+                updateImage();
+            });
+        } else { // senão ele apenas troca a imagem e troca a resposta, sem adicionar ao contador
+            botao.addEventListener('click', updateImage);
+        }
+    });
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-    // Seleciona todos os botões com a classe "opcao_btn"
-    const buttons = document.querySelectorAll('.opcao_btn');
 
     // Seleciona um por um os botões após o carregamento completo do context do site
     botao0 = document.getElementById('opcao_btn_0');
@@ -57,11 +90,6 @@ document.addEventListener('DOMContentLoaded', () => {
     botao2 = document.getElementById('opcao_btn_2');
     botao3 = document.getElementById('opcao_btn_3');
     contador = document.getElementById('contador');
-
-    // Adiciona um ouvinte de evento a cada botão
-    buttons.forEach(button => {
-        button.addEventListener('click', updateImage);
-    });
 
     // Atualiza a imagem quando a página carrega
     updateImage();
