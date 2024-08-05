a5c640f249ca21c77a6389174fbad36b0607dc49
DaviMunhoz1005 <davimunhoz1005@gmail.com>
2024-08-04 22:48:18 -0300

    Corrigindo botões do Html

--- pastas d testes/script.js
+++ pastas d testes/script.js
@@ -1,11 +1,11 @@
-const images = [
+const imagens = [
     { caminho: 'imagens/Bola.png', resposta: 'Bola' },
     { caminho: 'imagens/Carro.png', resposta: 'Carro' },
     { caminho: 'imagens/Cubo.png', resposta: 'Cubo' },
     { caminho: 'imagens/Estrela.png', resposta: 'Estrela' },
 ];
 
-let btn0, btn1, btn2, btn3;
+let botao0, botao1, botao2, botao3;
 
 let respostas = [];
 
@@ -17,19 +17,20 @@ function getRandomIndex(arr) {
 // Função para atualizar a imagem
 function updateImage() {
     const imageElement = document.getElementById('imagem_qst');
-    const image = images[getRandomIndex(images)];
-    const randomImage = image.caminho;
-    respostas.push(image.resposta);
+    const imagen = imagens[getRandomIndex(imagens)];
+    const randomImage = imagen.caminho;
+    respostas.push(imagen.resposta);
     pegarRespostas();
     imageElement.src = randomImage;
     Respostas_btn();
 }
 
 function pegarRespostas() {
+    // enquanto o tamanho do array de respostas for menor que 4 ele vai ficar fazendo essa rotina
     while (respostas.length < 4) {
-        let image = images[getRandomIndex(images)];
-        if (!respostas.includes(image.resposta)) {
-            respostas.push(image.resposta);
+        let imagen = imagens[getRandomIndex(imagens)];
+        if (!respostas.includes(imagen.resposta)) {
+            respostas.push(imagen.resposta);
         }
     }
 
@@ -40,19 +41,21 @@ function pegarRespostas() {
 
 function Respostas_btn() {
     // Seleciona os botões pelo ID e altera o texto deles
-    btn0.textContent = respostas[0];
-    btn1.textContent = respostas[1];
-    btn2.textContent = respostas[2];
-    btn3.textContent = respostas[3];
+    botao0.textContent = respostas[0];
+    botao1.textContent = respostas[1];
+    botao2.textContent = respostas[2];
+    botao3.textContent = respostas[3];
 }
 
 document.addEventListener('DOMContentLoaded', () => {
     // Seleciona todos os botões com a classe "opcao_btn"
     const buttons = document.querySelectorAll('.opcao_btn');
-    btn0 = document.getElementById('opcao_btn_0');
-    btn1 = document.getElementById('opcao_btn_1');
-    btn2 = document.getElementById('opcao_btn_2');
-    btn3 = document.getElementById('opcao_btn_3');
+
+    // Seleciona um por um os botões após o carregamento completo do context do site
+    botao0 = document.getElementById('opcao_btn_0');
+    botao1 = document.getElementById('opcao_btn_1');
+    botao2 = document.getElementById('opcao_btn_2');
+    botao3 = document.getElementById('opcao_btn_3');
 
     // Adiciona um ouvinte de evento a cada botão
     buttons.forEach(button => {
