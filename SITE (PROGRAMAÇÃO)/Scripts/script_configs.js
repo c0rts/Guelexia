ed35dd8791f63b37ff2f05093ca65cdd2c11458d
C0rts <matheuscbarbeiro30@gmail.com>
2024-08-13 16:06:13 -0300

    tela de configuração, apenas protótipo

---
+++ SITE (PROGRAMAÇÃO)/Scripts/script_configs.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const button = document.getElementById('tema');
+    const body = document.body;
+
+    // Verifica o tema atual e aplica a classe correspondente
+    if (localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-theme');
+        button.textContent = 'Mudar para Claro';
+    } else {
+        body.classList.add('light-theme');
+        button.textContent = 'Mudar para Escuro';
+    }
+
+    button.addEventListener('click', () => {
+        if (body.classList.contains('light-theme')) {
+            body.classList.remove('light-theme');
+            body.classList.add('dark-theme');
+            button.textContent = 'Mudar para Claro';
+            localStorage.setItem('theme', 'dark');
+        } else {
+            body.classList.remove('dark-theme');
+            body.classList.add('light-theme');
+            button.textContent = 'Mudar para Escuro';
+            localStorage.setItem('theme', 'light');
+        }
+    });
+});
